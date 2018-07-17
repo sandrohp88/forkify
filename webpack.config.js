71f27a91dd3a4ec794ad0c5f94fa57b2b7bb8d82
Sandro Herrera Pallares <sandrohp88@gmail.com>
2018-07-17 12:35:59 -0700

    Blank app setup with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: ["babel-polyfill","./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js"
+  },
+  mode: "development",
+  devServer: {
+    contentBase: "./dist"
+  },
+  // Create a new index.html in the dist folder every time the project compiles
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
