88043435a9c12117e15244f762ab2efc119310c2
Sandro Herrera Pallares <sandrohp88@gmail.com>
2018-07-18 13:51:32 -0700

    Limit recipes title to a defaclt length of 17 characters

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,15 +1,29 @@
 import { UIElements } from "./domObjects";
 export const getSearchInput = () => UIElements.searchInput.value;
-
+const limitRecipeTitle = (title, limit = 17) => {
+  const titleWords = title.split(" ");
+  let newTitle = "";
+  titleWords.reduce((accumulator, currentTitle) => {
+    if (currentTitle.length + accumulator <= limit) {
+      newTitle += currentTitle + " ";
+    }
+    return accumulator + newTitle.length;
+  }, 0);
+  return newTitle + "...";
+};
 const renderRecipe = recipe => {
   const markup = `
         <li>
             <a class="results__link" href="#${recipe.recipe_id}">
                 <figure class="results__fig">
-                    <img src="${recipe.image_url}" alt="${recipe.title}">
+                    <img src="${recipe.image_url}" alt="${limitRecipeTitle(
+    recipe.title
+  )}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(
+                      recipe.title
+                    )}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
