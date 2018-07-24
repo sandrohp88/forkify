7073b95008fc971b6d78b62c53a10fa22c42bb12
Sandro Herrera Pallares <sandrohp88@gmail.com>
2018-07-24 10:04:57 -0700

    Recipe parse finished, moving to recipeView

--- src/js/index.js
+++ src/js/index.js
@@ -15,59 +15,64 @@ const state = {};
  * Search controller
  */
 const controlSearch = async () => {
-  // 1- Get query from the view
-  const query = searchView.getSearchInput(); // TODO
-  if (query) {
-    // 2- New search object and add to state
-    state.search = new Search(query);
+	// 1- Get query from the view
+	const query = searchView.getSearchInput(); // TODO
+	if (query) {
+		// 2- New search object and add to state
+		state.search = new Search(query);
 
-    // 3- Prepare UI for results TODO
-    searchView.clearInput();
-    searchView.clearRecipesList();
-    // 4- Search for recipes
-    renderSpinner(UIElements.result);
-    await state.search.getResults();
+		// 3- Prepare UI for results TODO
+		searchView.clearInput();
+		searchView.clearRecipesList();
+		// 4- Search for recipes
+		renderSpinner(UIElements.result);
+		await state.search.getResults();
 
-    // 5- Render the UI
-    removeSpinner();
-    searchView.renderRecipes(state.search.recipes);
-  }
+		// 5- Render the UI
+		removeSpinner();
+		searchView.renderRecipes(state.search.recipes);
+	}
 };
 
 UIElements.searchForm.addEventListener('submit', event => {
-  event.preventDefault();
-  controlSearch();
+	event.preventDefault();
+	controlSearch();
 });
 
 UIElements.resultPages.addEventListener('click', event => {
-  const button = event.target.closest('.btn-inline');
-  if (button) {
-    const gotoPage = parseInt(button.dataset.goto);
-    searchView.clearRecipesList();
-    searchView.renderRecipes(state.search.recipes, gotoPage);
-  }
+	const button = event.target.closest('.btn-inline');
+	if (button) {
+		const gotoPage = parseInt(button.dataset.goto);
+		searchView.clearRecipesList();
+		searchView.renderRecipes(state.search.recipes, gotoPage);
+	}
 });
+
+/**
+ * Recipe controller
+ */
 const controlRecipe = async () => {
-  const recipeID = window.location.hash.replace('#', '');
-  if (recipeID) {
-    // Prepare UI for changes
-    // Create a new recipe Object
-    state.recipe = new Recipe(recipeID);
+	const recipeID = window.location.hash.replace('#', '');
+	if (recipeID) {
+		// Prepare UI for changes
+		// Create a new recipe Object
+		state.recipe = new Recipe(recipeID);
 
-    try {
-      // Get recipe data and parse ingredients
-      await state.recipe.getResults();
-      state.recipe.calculateTime();
-      state.recipe.calculateServings();
-    } catch (error) {
-      alert('Ooops something went wrong', error);
-    }
+		try {
+			// Get recipe data and parse ingredients
+			await state.recipe.getRecipe();
+			state.recipe.calculateTime();
+			state.recipe.calculateServings();
+			state.recipe.parseIngredients();
+		} catch (error) {
+			alert('Ooops something went wrong', error);
+		}
 
-    // render recipe
-    console.log(state.recipe);
-  }
+		// render recipe
+		// console.log(state.recipe.ingredients);
+	}
 };
 
 ['hashchange', 'load'].forEach(event =>
-  window.addEventListener(event, controlRecipe)
+	window.addEventListener(event, controlRecipe)
 );
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,34 +1,110 @@
-import axios from "axios";
-import { proxy, key, food2ForkGet } from "../../config";
+import axios from 'axios';
+import { proxy, key, food2ForkGet } from '../../config';
 
 export default class Recipe {
-  constructor(id) {
-    this.id = id;
-  }
+	constructor(id) {
+		this.id = id;
+	}
 
-  async getRecipe() {
-    try {
-      const response = await axios(
-        `${proxy}${food2ForkGet}key=${key}&rId=${this.id}`
-      );
-      this.title = response.data.recipe.title;
-      this.author = response.data.recipe.publisher;
-      this.img = response.data.recipe.image_url;
-      this.url = response.data.recipe.source_url;
-      this.ingredients = response.data.recipe.ingredients;
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	async getRecipe() {
+		try {
+			const response = await axios(
+				`${proxy}${food2ForkGet}key=${key}&rId=${this.id}`
+			);
+			this.title = response.data.recipe.title;
+			this.author = response.data.recipe.publisher;
+			this.img = response.data.recipe.image_url;
+			this.url = response.data.recipe.source_url;
+			this.ingredients = response.data.recipe.ingredients;
+		} catch (error) {
+			alert(error);
+		}
+	}
 
-  calculateTime() {
-    // Assuming that we need 15 min for each 3 ingredients
-    const numIng = this.ingredients.length;
-    const periods = Math.ceil(numIng / 3);
-    this.time = periods * 15;
-  }
+	calculateTime() {
+		// Assuming that we need 15 min for each 3 ingredients
+		const numIng = this.ingredients.length;
+		const periods = Math.ceil(numIng / 3);
+		this.time = periods * 15;
+	}
 
-  calculateServings() {
-    this.servings = 4;
-  }
+	calculateServings() {
+		this.servings = 4;
+	}
+
+	parseIngredients() {
+		const newIngredients = this.ingredients.map(currentIngredient => {
+			const unitsLong = [
+				'tablespoons',
+				'tablespoon',
+				'ounces',
+				'ounce',
+				'teaspoons',
+				'teaspoon',
+				'cups',
+				'pounds'
+			];
+			const unitsShort = [
+				'tbsp',
+				'tbsp',
+				'oz',
+				'oz',
+				'tsp',
+				'tsp',
+				'cup',
+				'pound'
+			];
+
+			// Uniform units
+			let ingredient = currentIngredient.toLowerCase();
+
+			unitsLong.forEach((unit, i) => {
+				ingredient = ingredient.replace(unit, unitsShort[i]);
+			});
+			// Remove parentheses
+			ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+			// Parse Ingredients into count, unit and ingredient
+			const arrayIngredient = ingredient.split(' ');
+			const unitIndex = arrayIngredient.findIndex(current =>
+				unitsShort.includes(current)
+			);
+			let objIngredient;
+			if (unitIndex > -1) {
+				// There is a unit
+				// Ex. 3 1/2 cups, arrayCount is [4,1/2] eval("4+1/2") = 4.5
+				// Ex. 4 cups, arrayCount is [4]
+				const arrayCount = arrayIngredient.slice(0, unitIndex);
+
+				let count;
+				if (arrayCount === 1) {
+					count = eval(arrayIngredient[0].replace('-', '+'));
+				} else {
+					count = eval(arrayIngredient.slice(0, unitIndex).join('+'));
+				}
+				objIngredient = {
+					count,
+					unit: arrayIngredient[unitIndex],
+					ingredient: arrayIngredient.slice(1).join(' ')
+				};
+			} else if (parseInt(arrayIngredient[0])) {
+				// There is no unit but first element it's a number
+				objIngredient = {
+					count: parseInt(arrayIngredient[0]),
+					unit: '',
+					ingredient: arrayIngredient.slice(1).join(' ')
+				};
+			} else if (unitIndex === -1) {
+				// There is no unit and no number in first position
+				objIngredient = {
+					count: 1,
+					unit: '',
+					ingredient
+				};
+			}
+			console.log(objIngredient);
+			return objIngredient;
+		});
+		this.ingredients = newIngredients;
+	}
 }
